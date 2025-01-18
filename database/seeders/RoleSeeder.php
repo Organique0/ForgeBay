57eb0d99c116ae91390762a104b1eb6165819c50
lukag <grabnar.luka@gmail.com>
2025-01-18 02:48:52 +0100

    Add Filament

---
+++ database/seeders/RoleSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\RolesEnum;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+
+class RoleSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $userRole = Role::create(['name' => RolesEnum::User->value]);
+        $adminRole = Role::create(['name' => RolesEnum::Admin->value]);
+
+        //you can also make permissions here and sync them with roles
+    }
+}
