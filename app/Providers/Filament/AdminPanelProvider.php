e4d29d9b624d23a42b801864ecb74a14e1aa688a
lukag <grabnar.luka@gmail.com>
2025-01-20 03:48:55 +0100

    not needed anymore

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -59,7 +59,7 @@ class AdminPanelProvider extends PanelProvider
                 DispatchServingFilamentEvent::class,
             ])
             ->authMiddleware([
-                CustomFilamentAuthenticate::class,
+				Authenticate::class
             ]);
     }
 }
