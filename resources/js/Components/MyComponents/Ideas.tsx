d4307634e2e60db3e0e0dc3991bc098019afab3a
Luka Grabnar <grabnar.luka@gmail.com>
2025-02-11 11:56:24 +0100

    ui changes

--- resources/js/Components/MyComponents/Ideas.tsx
+++ resources/js/Components/MyComponents/Ideas.tsx
@@ -11,24 +11,26 @@ const Ideas: React.FC<IdeasProps> = ({ ideas }) => {
 
 
 	return (
-		<div className={'grid gap-3 grid-cols-1 lg:grid-cols-2 xl:grid-cols-3'}>
+		<div className={''}>
 			{ideas.map((idea: Idea) => {
 				const totalValue = idea.tasks.reduce((acc, task) => acc + task.value, 0);
 				return (
-					<div key={idea.id}>
+					<div key={idea.id} className='my-6'>
 						<Link href={`/idea/${idea.id}`}>
 							<Card className={'min-h-[25em] flex flex-col'}>
 								<CardHeader>
-									<StatusPretty idea={idea} />
-									<div className={'text-2xl font-semibold'}>{idea.title}</div>
-									<p>${totalValue}</p>
+									<div className={'flex justify-between'}>
+										<h1 className='text-3xl font-semibold f'>{idea.title}</h1>
+										<StatusPretty idea={idea} className={'text-lg'} />
+									</div>
+									<p className="font-bold text-xl">${totalValue}</p>
 								</CardHeader>
 								<CardContent className={'grow'}>
 									<p className={'grow'}>{idea.description}</p>
 								</CardContent>
 								<CardFooter className={'gap-2'}>
 									{idea.tags.map((tag: Tag) => (
-										<Badge key={tag.id}>{tag.name}</Badge>
+										<Badge key={tag.id} className='text-lg'>{tag.name}</Badge>
 									))}
 								</CardFooter>
 							</Card>
